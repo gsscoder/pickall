d84e71b300b4c7cbd8fc51017768d0a51cef71fc
Giacomo Stelluti Scala <gsscoder@gmail.com>
2019-12-05 09:42:12 +0100

    Sample enhanced using Command Line Parser Library

--- samples/PickAll.Simple/Program.cs
+++ samples/PickAll.Simple/Program.cs
@@ -2,23 +2,34 @@
 using System.Linq;
 using System.Threading.Tasks;
 using PickAll.PostProcessors;
+using CommandLine;
 
 namespace PickAll.Simple
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static int Main(string[] args)
         {
-            var context = SearchContext.Default()
-                .With(new FuzzyMatch("Steve Jobs Biography", 10));
-            var results = await context.Search("steve jobs");
+            return Parser.Default.ParseArguments<Options>(args)
+                .MapResult(options => DoSearch(options).GetAwaiter().GetResult(),
+                _ => 1);
+        }
+
+        static async Task<int> DoSearch(Options options)
+        {
+            var context = SearchContext.Default();
+            if (!string.IsNullOrEmpty(options.FuzzyMatch)) {
+                context = context.With(new FuzzyMatch(options.FuzzyMatch, 10));
+            }
+            var results = await context.Search(options.Query);
             var filtered = results;
 
-            foreach(var result in filtered)
-            {
+            foreach(var result in filtered) {
                 Console.WriteLine(
                     $"[{result.Index}] {result.Originator}: \"{result.Description}\": \"{result.Url}\"");
             }
+
+            return 0;
         }
     }
 }
