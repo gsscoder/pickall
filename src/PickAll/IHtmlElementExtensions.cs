e64a13edda23df7fb9d85865ff60e82ef5a6136c
Giacomo Stelluti Scala <gsscoder@gmail.com>
2019-12-11 06:08:26 +0100

    Removed contracts assertion

--- src/PickAll/IHtmlElementExtensions.cs
+++ src/PickAll/IHtmlElementExtensions.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics.Contracts;
 using System.Linq;
 using AngleSharp.Dom;
 using AngleSharp.Html.Dom;
@@ -10,8 +9,6 @@ namespace PickAll
         public static string FirstChildText(this IHtmlElement element,
             params string[] selectorsGroup)
         {
-            Contract.Ensures(selectorsGroup != null && selectorsGroup.Length > 0);
-
             foreach (var selectors in selectorsGroup) {
                 var selected = element.QuerySelector(selectors);
                 if (selected != null) {
