c010d035c6f6ecfe181a736f1dfb6227c9b78944
Giacomo Stelluti Scala <gsscoder@gmail.com>
2019-12-19 08:02:50 +0100

    Added FSharp samples

---
+++ samples/FSharp/pickall_count_words.fsx
@@ -0,0 +1,34 @@
+﻿(*
+* pickall_count_words.fsx
+* - F# Script that demonstrates the use of PickAll (github.com/gsscoder/pickall)
+* - Searches the web and counts the words of results descriptions
+* - Requires PickAll.dll (0.8.0) and AngleSharp.dll (0.14.0-alpha-787)
+*   in the script directory
+*)
+#r "PickAll.dll"
+open System
+open Microsoft.FSharp.Control
+open PickAll
+open PickAll.Searchers
+
+let alpha (s : string) =
+   not (s.ToCharArray() 
+   |> Seq.map (fun x -> Char.IsLetterOrDigit(x) && not (Char.IsWhiteSpace(x)))
+   |> Seq.contains false)
+
+let query = "Steve Jobs"
+
+let context = (new SearchContext())
+               .With<Google>()
+               .With<DuckDuckGo>()
+               .With<Yahoo>()
+let results = context.SearchAsync(query)
+              |> Async.AwaitTask
+              |> Async.RunSynchronously
+let words = results
+           |> Seq.map (fun x -> x.Description.Split())
+           |> Seq.concat
+words |> Seq.filter alpha 
+      |> Seq.countBy id
+      |> Seq.sortBy (fun (_, y) -> -y)
+      |> Seq.iter (fun (x, y) -> printfn "%s %d" x y)
---
+++ samples/FSharp/pickall_search_of_searches.fsx
@@ -0,0 +1,47 @@
+﻿(*
+* pickall_search_of_searches.fsx
+* - F# Script that demonstrates the use of PickAll (github.com/gsscoder/pickall)
+* - Searches the web adding the first two more used words in initial results
+* - descriptions for a subsequent query
+* - Requires PickAll.dll (0.8.0) and AngleSharp.dll (0.14.0-alpha-787)
+*   in the script directory
+*)
+#r "PickAll.dll"
+open System
+open Microsoft.FSharp.Control
+open PickAll
+open PickAll.Searchers
+open PickAll.PostProcessors
+
+let alpha (s : string) =
+   not (s.ToCharArray() 
+   |> Seq.map (fun x -> Char.IsLetterOrDigit(x) && not (Char.IsWhiteSpace(x)))
+   |> Seq.contains false)
+
+let query = "Bill Gates"
+
+let context = (new SearchContext())
+               .With<Google>()
+               .With<DuckDuckGo>()
+               .With<Yahoo>()
+let results = context.SearchAsync(query)
+              |> Async.AwaitTask
+              |> Async.RunSynchronously
+let words = results
+            |> Seq.map (fun x -> x.Description.Split())
+            |> Seq.concat
+let exclude = query.Split()
+let newQuery = (words |> Seq.filter alpha
+            |> Seq.filter (fun x -> exclude |> Seq.contains x |> not)
+            |> Seq.countBy id
+            |> Seq.sortBy (fun (_, y) -> -y)
+            |> Seq.take 2
+            |> Seq.map (fun (x, _) -> x + " ")
+            |> String.Concat).Trim() + " " + query
+let newContext = context
+                  .With<Uniqueness>()
+                  .With<Order>()
+newContext.SearchAsync(newQuery)
+    |> Async.AwaitTask
+    |> Async.RunSynchronously
+    |> Seq.iter (fun x -> printfn "%s - %s" (x.Description.ToUpper()) x.Url)
