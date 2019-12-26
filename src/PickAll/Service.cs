e6aaaa7ff4bece678d5d7772df4c08267d7921d5
SimonCropp <simon.cropp@gmail.com>
2019-12-26 21:44:52 +1100

    use props

--- src/PickAll/Service.cs
+++ src/PickAll/Service.cs
@@ -2,19 +2,7 @@ namespace PickAll
 {
     public abstract class Service
     {
-        private SearchContext _context;
-        private object _settings;
-
-        public SearchContext Context
-        {
-            get { return _context; }
-            set { _context = value; }
-        }
-
-        protected object Settings
-        {
-            get { return _settings; }
-            set { _settings = value; }
-        }
+        public SearchContext Context { get; set; }
+        protected object Settings { get; set; }
     }
 }
