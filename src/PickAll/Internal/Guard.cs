bb2f1af9fbbd4bd60e9d77b622f012e3cdc4a869
Giacomo Stelluti Scala <gsscoder@gmail.com>
2020-01-21 09:32:35 +0100

    Added AggressiveInlining in Guard class

--- src/PickAll/Internal/Guard.cs
+++ src/PickAll/Internal/Guard.cs
@@ -1,13 +1,16 @@
 using System;
+using System.Runtime.CompilerServices;
 
 static class Guard
 {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void AgainstNull(string argumentName, object value)
     {
         if (value == null) throw new ArgumentNullException(argumentName,
             $"{argumentName} cannot be null");
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void AgainstEmptyWhiteSpace(string argumentName, string value)
     {
         if (value.Trim() == string.Empty) throw new ArgumentException(
