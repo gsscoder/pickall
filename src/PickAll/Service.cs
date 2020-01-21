d1e4fbfaa284e8afbbb68f18388183df5cf95053
Giacomo Stelluti Scala <gsscoder@gmail.com>
2020-01-21 09:54:04 +0100

    Used automatic properties in Service

--- src/PickAll/Service.cs
+++ src/PickAll/Service.cs
@@ -1,20 +1,10 @@
+using System;
+
 namespace PickAll
 {
     public abstract class Service
     {
-        private SearchContext _context;
-        private object _settings;
-
-        public SearchContext Context
-        {
-            get { return _context; }
-            set { _context = value; }
-        }
-
-        protected object Settings
-        {
-            get { return _settings; }
-            set { _settings = value; }
-        }
+        public SearchContext Context { get; set; }
+        protected object Settings { get; set; }
     }
 }
