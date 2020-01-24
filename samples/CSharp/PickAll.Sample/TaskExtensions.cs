da2020b9b7ff8dd8c2749287d5798f710794c36f
Giacomo Stelluti Scala <gsscoder@gmail.com>
2020-01-24 17:35:25 +0100

    Aligning TaskExtensions.cs files

--- samples/CSharp/PickAll.Sample/TaskExtensions.cs
+++ samples/CSharp/PickAll.Sample/TaskExtensions.cs
@@ -1,7 +1,9 @@
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 static class TaskExtensions
 {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static T RunSynchronously<T>(this Task<T> task)
     {
         return task.GetAwaiter().GetResult();
