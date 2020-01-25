0a395d60c109ef144fbdf4a662c9f52df2fee6b2
Giacomo Stelluti Scala <gsscoder@gmail.com>
2020-01-25 12:03:29 +0100

    Tidying up XML comments in ContextSettings

--- src/PickAll/ContextSettings.cs
+++ src/PickAll/ContextSettings.cs
@@ -2,16 +2,12 @@ using System;
 
 namespace PickAll
 {
-    /// <summary>
-    /// Settings for a search context.
-    /// </summary>
+    /// <summary>Settings for a search context.</summary>
     public struct ContextSettings
     {
         int? _maximumResults;
     
-        /// <summary>
-        /// Maximum results a search is allowed to return.
-        /// </summary>
+        /// <summary>Maximum results a search is allowed to return.</summary>
         public int? MaximumResults
         {
             get { return _maximumResults; }
@@ -22,14 +18,10 @@ namespace PickAll
             }
         }
 
-        /// <summary>
-        /// Timeout for each HTTP request performed.
-        /// </summary>
+        /// <summary>Timeout for each HTTP request performed.</summary>
         public TimeSpan? Timeout;
 
-        /// <summary>
-        /// Enables events in search context and services.
-        /// </summary>
+        /// <summary>Enables events in search context and services.</summary>
         public bool EnableRaisingEvents;
 
         internal RuntimePolicy ToPolicy()
