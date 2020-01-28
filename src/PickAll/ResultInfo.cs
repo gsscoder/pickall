b8231c26ff4b3838dae95d67c80733b5860ec70e
Giacomo Stelluti Scala <gsscoder@gmail.com>
2020-01-28 11:01:51 +0100

    Tidying up XML comments in ResultInfo

--- src/PickAll/ResultInfo.cs
+++ src/PickAll/ResultInfo.cs
@@ -1,99 +1,80 @@
 namespace PickAll
 {
-        /// <summary>
-        /// Models an<see cref="Searcher"/> result record.
-        /// </summary>
-        public class ResultInfo
-        {
+    /// <summary>Models an <c>Searcher<c/> result record.</summary>
+    public class ResultInfo
+    {
         #if DEBUG
         public ResultInfo() { }
         #endif
-        /// <summary>
-        /// Initializes a new instance of <see cref="ResultInfo"/>.
-        /// </summary>
-        /// <param name="originator">The searcher which originated the result.</param>
-        /// <param name="index">The result index.</param>
-        /// <param name="url">The result URL.</param>
-        /// <param name="description">The result description.</param>
-        /// <param name="data">TAdditional data supplied by the service.</param>
+        /// <summary>Initializes a new instance of <c>ResultInfo<c/>.</summary>
         public ResultInfo(string originator, int index, string url, string description, object data)
         {
-                Guard.AgainstNull(nameof(originator), originator);
-                Guard.AgainstEmptyWhiteSpace(nameof(originator), originator);
-                Guard.AgainstNegative(nameof(index), index);
-                Guard.AgainstNull(nameof(url), url);
-                Guard.AgainstEmptyWhiteSpace(nameof(url), url);
-                Guard.AgainstNull(nameof(description), description);
+            Guard.AgainstNull(nameof(originator), originator);
+            Guard.AgainstEmptyWhiteSpace(nameof(originator), originator);
+            Guard.AgainstNegative(nameof(index), index);
+            Guard.AgainstNull(nameof(url), url);
+            Guard.AgainstEmptyWhiteSpace(nameof(url), url);
+            Guard.AgainstNull(nameof(description), description);
 
-                Originator = originator;
-                Index = index;
-                Url = url;
-                Description = description;
-                Data = data;
+            Originator = originator;
+            Index = index;
+            Url = url;
+            Description = description;
+            Data = data;
         }
 
-        /// <summary>
-        /// The searcher which originated the result.
-        /// </summary>
+        /// <summary>The searcher which originated the result.</summary>
         public string Originator
         {
-                get;
+            get;
         #if DEBUG
-                private set;
+            private set;
         #else
-                internal set;
+            internal set;
         #endif
         }
 
-        /// <summary>
-        /// The result index.
-        /// </summary>
+        /// <summary>The result index.</summary>
         public int Index
         {
-                get;
+            get;
         #if !DEBUG
-                private set;
+            private set;
         #else
-                internal set;
+            internal set;
         #endif
         }
 
-        /// <summary>
-        /// The result URL
-        /// </summary>
+        /// <summary>The result URL.</summary>
         public string Url
         {
-                get;
+            get;
         #if !DEBUG
-                private set;
+            private set;
         #else
-                internal set;
+            internal set;
         #endif
         }
 
-        /// <summary>
-        /// The result description.
-        /// </summary>
+        /// <summary>The result description.</summary>
         public string Description 
         {
-                get;
+            get;
         #if !DEBUG
-                private set;
+            private set;
         #else
-                internal set;
+            internal set;
         #endif
         }
-        /// <summary>
-        /// Additional data supplied by the service. 
-        /// </summary>
+        /// <summary>Additional data supplied by the service.</summary>
         public object Data
         {
-                get;
+            get;
         #if !DEBUG
-                private set;
+            private set;
         #else
-                internal set;
+            internal set;
         #endif
         }
-        } 
+    }
 }
