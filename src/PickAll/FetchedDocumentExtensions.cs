2fd638634e9980ab50974f55bf13dedc49855744
Giacomo Stelluti Scala <gsscoder@gmail.com>
2020-02-01 06:57:48 +0100

    Added first arg checks in FetchedDocumentExtensions

--- src/PickAll/FetchedDocumentExtensions.cs
+++ src/PickAll/FetchedDocumentExtensions.cs
@@ -8,11 +8,14 @@ namespace PickAll
     {
         public static string ElementSelector(this IFetchedDocument document, string tag)
         {
+            Guard.AgainstNull(nameof(document), document);
+
             return document.ElementSelectorAll(tag).SingleOrDefault() ?? string.Empty;
         }
 
         public static IEnumerable<string> ElementSelectorAll(this IFetchedDocument document, string tag)
         {
+            Guard.AgainstNull(nameof(document), document);
             Guard.AgainstNull(nameof(tag), tag);
             Guard.AgainstEmptyWhiteSpace(nameof(tag), tag);
 
