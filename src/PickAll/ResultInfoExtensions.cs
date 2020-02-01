a2ade0ec08b7b198142b7b7ad17f8a4019ce5909
Giacomo Stelluti Scala <gsscoder@gmail.com>
2020-02-01 06:56:08 +0100

    Added first arg check in ResultInfoExtensions.Clone

--- src/PickAll/ResultInfoExtensions.cs
+++ src/PickAll/ResultInfoExtensions.cs
@@ -4,6 +4,8 @@ namespace PickAll
     {
         public static ResultInfo Clone(this ResultInfo resultInfo, object data = null)
         {
+            Guard.AgainstNull(nameof(resultInfo), resultInfo);
+
             var _data = data ?? resultInfo.Data;
             return new ResultInfo(
                 resultInfo.Originator,
