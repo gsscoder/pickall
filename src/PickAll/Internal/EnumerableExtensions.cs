aeffe1c593cab40bd042e0a21e84a803cc8e7c10
Giacomo Stelluti Scala <gsscoder@gmail.com>
2020-02-01 07:04:47 +0100

    Removed unused method from EnumerableExtensions

--- src/PickAll/Internal/EnumerableExtensions.cs
+++ src/PickAll/Internal/EnumerableExtensions.cs
@@ -1,12 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 static class EnumerableExtensions
 {
-    public static IEnumerable<T> Memoize<T>(
-        this IEnumerable<T> source) => source.GetType().IsArray ? source : source.ToArray();
-
     public static IEnumerable<TSource> DistinctBy<TSource, TKey>(
         this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
     {
