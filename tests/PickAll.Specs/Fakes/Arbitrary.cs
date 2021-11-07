763b09f3502384007b1c1b6dbc9402756c9f3f35
Giacomo Stelluti Scala <gsscoder@gmail.com>
2021-11-07 07:12:16 +0100

    Refactored out useless helper in specs project

--- tests/PickAll.Specs/Fakes/Arbitrary.cs
+++ tests/PickAll.Specs/Fakes/Arbitrary.cs
@@ -80,7 +80,7 @@ static class WaffleBuilder
                 WaffleEngine.Html(
                     paragraphs: paragraphs,
                     includeHeading: true,
-                    includeHeadAndBody: true))).RunSynchronously<IDocument>();
+                    includeHeadAndBody: true))).Result;
     }
 
     public static string GenerateParagraph(int samples = 1, bool small = false)
