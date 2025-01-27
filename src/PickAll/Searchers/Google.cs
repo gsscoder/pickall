26e6ebf00b44d6dbe8440fec80ceffb2a9329446
Giacomo Stelluti Scala <gsscoder@gmail.com>
2025-01-27 17:36:09 +0100

    staging

--- src/PickAll/Searchers/Google.cs
+++ src/PickAll/Searchers/Google.cs
@@ -1,70 +1,53 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
-using HtmlAgilityPack;
-using PickAll.Searchers;
-using PuppeteerSharp;
-using SharpX.Extensions;
+using AngleSharp;
+using AngleSharp.Html.Dom;
+using AngleSharp.Dom;
 
-namespace PickAll;
-
-/// <summary><c>Searcher</c> that searches on Google search engine.</summary>
-public class Google : Searcher
+namespace PickAll
 {
-    static readonly Regex _normalize = new Regex(@"^/url\?q=([^&]*)&.*", RegexOptions.Compiled);
-
-    public Google(object settings) : base(settings)  
+    /// <summary><c>Searcher</c> that searches on Google search engine.</summary>
+    public class Google : Searcher
     {
-    }
+        static readonly Regex _normalize = new Regex(@"^/url\?q=([^&]*)&.*", RegexOptions.Compiled);
 
-    public override async Task<IEnumerable<ResultInfo>> SearchAsync(string query)
-    {
-        var page = await Context.HeadlessBrowsing.NewPageAsync();
-        await page.UseStealthMode();
-        var url = $"https://www.google.com/search?q={Uri.EscapeDataString(query)}";
-        var response = await page.GoToAsync(url, new NavigationOptions
+        public Google(object settings) : base(settings)
         {
-            WaitUntil = [WaitUntilNavigation.Load]
-        });
-        response.EnsureSuccessOrThrow(
-            new SearcherException("Unable to navigate to 'https://www.google.com/search?q={query}'."));
-
-        var htmlContent = await page.EvaluateFunctionAsync<string>(
-            "document.querySelector('#center_col').outerHTML");
-        if (htmlContent == null) {
-            throw new SearcherException($"Unable to select item 'center_col'.");
         }
-        var htmlDoc = new HtmlDocument();
-        htmlDoc.LoadHtml(htmlContent);
 
-        var links = htmlDoc.DocumentNode.SelectNodes("//a")
-            .Where(x => !x.GetAttributeValue("href", string.Empty).IsEmpty());
+        public override async Task<IEnumerable<ResultInfo>> SearchAsync(string query)
+        {
+            using var document = await Context.Browsing.OpenAsync("https://www.google.com/");
+            var form = document.QuerySelector<IHtmlFormElement>("form[action='/search']");
+            using var result = await form.SubmitAsync(new { q = query });
+            // Take only valid URLs
+            var links = from anchor in result.QuerySelectorAll<IHtmlAnchorElement>("a")
+                        where Validate(anchor.Attributes["href"].Value)
+                        select anchor;
+            // Create results normalizing URLs
+            var results = links.Select((link, index) =>
+                CreateResult((ushort)index, Normalize(link.Attributes["href"].Value),
+                    link.FirstChildText("div", "span")));
 
-        //using var result = await form.SubmitAsync(new { q = query });
-        //// Take only valid URLs
-        //var links = from anchor in result.QuerySelectorAll<IHtmlAnchorElement>("a")
-        //            where Validate(anchor.Attributes["href"].Value)
-        //            select anchor;
-        //// Create results normalizing URLs
-        //var results = links.Select((link, index) =>
-        //    CreateResult((ushort)index, Normalize(link.Attributes["href"].Value),
-        //        link.FirstChildText("div", "span")));
+            // Discard ones without description (not actual results)
+            return from @this in results
+                   where @this.Description.Trim() != string.Empty
+                   select @this;
+        }
 
-        //// Discard ones without description (not actual results)
-        //return from @this in results
-        //       where @this.Description.Trim() != string.Empty
-        //       select @this;
+        static bool Validate(string url) =>
+                url.StartsWith(
+                    "/url?", StringComparison.OrdinalIgnoreCase) &&
+                !url.StartsWith(
+                    "/url?q=http://webcache.googleusercontent.com", StringComparison.OrdinalIgnoreCase);
 
-        return Enumerable.Empty<ResultInfo>();
-    }
-
-    static bool Validate(string url) =>
-            url.StartsWith(
-                "/url?", StringComparison.OrdinalIgnoreCase) &&
-            !url.StartsWith(
-                "/url?q=http://webcache.googleusercontent.com",StringComparison.OrdinalIgnoreCase);
-
-    static string Normalize(string url)
-    {
-        var match = _normalize.Match(url);
-        return match.Groups.Count == 2 ? match.Groups[1].Value : url;
+        static string Normalize(string url)
+        {
+            var match = _normalize.Match(url);
+            return match.Groups.Count == 2 ? match.Groups[1].Value : url;
+        }
     }
 }
--- tests/PickAll.Specs/Tests/SearchersSpecs.cs
+++ tests/PickAll.Specs/Tests/SearchersSpecs.cs
@@ -7,17 +7,6 @@ namespace Tests;
 public class SearchersTests
 {
     [Fact]
-    public async Task Test_Google()
-    {
-        using var sut = new SearchContext()
-            .With("Google");
-
-        var results = await sut.SearchAsync("all about Mike Tyson");
-
-        results.Should().NotBeEmpty();
-    }
-
-    [Fact]
     public async Task Test_Bing()
     {
         using var sut = new SearchContext()
