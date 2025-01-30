73eada19bb846328c27ef07323678b5632d01bb0
Giacomo Stelluti Scala <gsscoder@gmail.com>
2025-01-30 20:19:11 +0100

    Updated Bing searcher class

--- src/PickAll/Searchers/Bing.cs
+++ src/PickAll/Searchers/Bing.cs
@@ -1,22 +1,17 @@
+using System.Data;
 using HtmlAgilityPack;
-using System.Net.Http;
 using PickAll.Searchers;
 using PuppeteerSharp;
-using System.Data;
 using SharpX.Extensions;
 
 namespace PickAll;
 
 /// <summary><c>Searcher</c> that searches on Bing search engine.</summary>
-public class Bing : Searcher
+public class Bing(object settings) : Searcher(settings)
 {
-    public Bing(object settings) : base(settings)  
-    {
-    }
-
     public override async Task<IEnumerable<ResultInfo>> SearchAsync(string query)
     {
-        var page = await Context.HeadlessBrowsing.NewPageAsync();
+        var page = await Context!.HeadlessBrowsing.NewPageAsync();
         await page.UseStealthMode();
         var url = $"https://www.bing.com/search?q={Uri.EscapeDataString(query)}";
         var response = await page.GoToAsync(url, new NavigationOptions
@@ -26,10 +21,9 @@ public class Bing : Searcher
         response.EnsureSuccessOrThrow(
             new SearcherException("Unable to navigate to 'https://www.google.com/search?q={query}'."));
 
-        var olHtmlContent = await page.EvaluateExpressionAsync<string>("document.querySelector('ol#b_results').outerHTML");
-        if (olHtmlContent == null) {
-            throw new SearcherException($"Unable to select item 'b_results'.");
-        }
+        var olHtmlContent =
+            await page.EvaluateExpressionAsync<string>("document.querySelector('ol#b_results').outerHTML")
+            ?? throw new SearcherException($"Unable to select item 'b_results'.");
         var resultsHtml = new HtmlDocument();
         resultsHtml.LoadHtml(olHtmlContent);
 
